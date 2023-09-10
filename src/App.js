05923845e0714ae6cae2a8a9129ecbaca23ffede
김유찬 <dkejdlcl123@naver.com>
2023-09-10 21:19:38 +0900

    기초 JSX 문법 3가지

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let post = "전주 비빕밥 맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: "orange", fontSize: "16px" }}>BLOG</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
