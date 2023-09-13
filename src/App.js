40ab5b881209e7a6be8e9e8a028ea091f38f4cd4
김유찬 <dkejdlcl123@naver.com>
2023-09-13 21:02:55 +0900

    props 사용

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       </button>
       {글제목.map(function (a, i) {
         return (
-          <div className="list">
+          <div className="list" key={i}>
             <h4
               onClick={() => {
                 setModal(!modal);
@@ -60,7 +60,9 @@ function App() {
           </div>
         );
       })}
-      {modal == true ? <Modal /> : null}
+      {modal == true ? (
+        <Modal color="skyblue" 글제목={글제목} 제목변경={제목변경} />
+      ) : null}
       {/* <div className="list">
         <h4>
           {글제목[0]}
@@ -106,12 +108,21 @@ function App() {
 // 컴포넌트: function만들기 -> return 내부 작성 -> <함수명></함수명>으로 사용
 // 반복적인 html 축약, 큰 페이지들, 자주 변경되는 것들
 // 컴포넌트 단점: state 가져다쓸 때 문제가 생기게 됨
-const Modal = () => {
+const Modal = (props) => {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{ backgroundColor: props.color }}>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.글제목]; // 카피본
+          copy[0] = "여자 아우터 추천";
+          props.제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
 };
